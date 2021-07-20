a4af91fb96e09cd927ce809829fe6f6662872484
Mariel Couto <coutomariel@gmail.com>
2021-07-20 16:31:02 -0300

    First commit

---
+++ fretes-rest/src/main/kotlin/br/com/zupacademy/CalculadoraDeFretesController.kt
@@ -0,0 +1,58 @@
+package br.com.zupacademy
+
+import com.google.protobuf.Any
+import com.google.rpc.ErrorDetailsProto
+import io.grpc.Status
+import io.grpc.StatusException
+import io.grpc.protobuf.StatusProto
+import io.micronaut.http.HttpStatus
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.QueryValue
+import io.micronaut.http.exceptions.HttpStatusException
+import javax.inject.Inject
+
+@Controller
+class CalculadoraDeFretesController(@Inject val gRpcClient: FretesServiceGrpc.FretesServiceBlockingStub) {
+
+    @Get("api/fretes")
+    fun calcula(@QueryValue cep: String): FreteResponse {
+
+        val request = CalculaFreteRequest.newBuilder().setCep(cep).build()
+
+        try {
+            val response = gRpcClient.calculaFrete(request)
+            return FreteResponse(
+                cep = response.cep,
+                valor = response.valor
+            )
+        } catch (e: StatusException) {
+            val statusCode = e.status.code
+            val description = e.status.description
+
+            if (statusCode == Status.Code.INVALID_ARGUMENT) {
+                throw HttpStatusException(HttpStatus.BAD_REQUEST, description)
+            }
+
+            if (statusCode == Status.Code.PERMISSION_DENIED) {
+
+                val statusProto = StatusProto.fromThrowable(e)
+                if (statusProto == null) {
+                    throw  HttpStatusException(HttpStatus.FORBIDDEN, description)
+                }
+
+                val anyDetails: Any = statusProto.detailsList.get(0)
+                val errorDetails = anyDetails.unpack(ErrorDetails::class.java)
+
+                throw HttpStatusException(HttpStatus.FORBIDDEN, "${errorDetails.code} : ${errorDetails.description}")
+            }
+
+            throw HttpStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.message)
+        }
+
+    }
+}
+
+data class FreteResponse(val cep: String, val valor: Double) {
+
+}
---
+++ fretes/src/main/kotlin/br/com/zupacademy/FretesRpcServer.kt
@@ -0,0 +1,83 @@
+package br.com.zupacademy
+
+import com.google.protobuf.Any
+import com.google.rpc.Code
+import io.grpc.Status
+import io.grpc.protobuf.StatusProto
+import io.grpc.stub.StreamObserver
+import org.slf4j.LoggerFactory
+import javax.inject.Singleton
+import kotlin.random.Random
+
+@Singleton
+class FretesRpcServer : FretesServiceGrpc.FretesServiceImplBase() {
+
+    private val logger = LoggerFactory.getLogger(FretesRpcServer::class.java)
+
+    override fun calculaFrete(request: CalculaFreteRequest?, responseObserver: StreamObserver<CalculaFreteResponse>?) {
+        logger.info("Calculando frete para o request: $request")
+
+        val cep = request?.cep
+        if (cep == null || cep.isBlank()) {
+            val e = Status.INVALID_ARGUMENT
+                .withDescription("CEP deve ser informado")
+                .asRuntimeException()
+
+            responseObserver?.onError(e)
+        }
+//
+        if (!cep!!.matches("[0-9]{5}-[0-9]{3}".toRegex())) {
+            val e = Status.INVALID_ARGUMENT
+                .withDescription("CEP inválido")
+                .augmentDescription("Cep precisa respeitar o formado 99999-999")
+                .asRuntimeException()
+
+            responseObserver?.onError(e)
+        }
+
+        // SIMULAÇÃO DE VERIFICAÇÃO DE SEGURANÇA
+        if (cep.endsWith("333")) {
+            val statusProto = com.google.rpc.Status.newBuilder()
+                .setCode(Code.PERMISSION_DENIED.number)
+                .setMessage("Usuário não pode acessar este recurso")
+                .addDetails(Any.pack(
+                    ErrorDetails.newBuilder()
+                    .setCode(401)
+                    .setMessage("Token expirado")
+                    .build())
+                )
+                .build()
+
+            val e = StatusProto.toStatusRuntimeException(statusProto)
+            responseObserver?.onError(e)
+        }
+
+        var valor = 0.0
+        try {
+            valor = Random.nextDouble(from = 0.0, until = 140.0)
+            if (valor > 100) {
+                throw IllegalArgumentException("Erro inesperado no sistema com valor: $valor")
+            }
+
+        } catch (e: Exception) {
+            responseObserver
+                ?.onError(
+                    Status.INTERNAL
+                        .withDescription(e.message)
+                        .withCause(e).asRuntimeException()
+                )
+        }
+
+
+        val response = CalculaFreteResponse.newBuilder()
+            .setCep(request!!.cep)
+            .setValor(valor)
+            .build()
+
+        logger.info("Frete calculado: $response")
+
+        responseObserver!!.onNext(response)
+        responseObserver.onCompleted()
+
+    }
+}
